23815ef98fde499c2298349ab5164af8475aebfe
Chris Mytton <chrismytton@gmail.com>
2015-11-26 16:28:18 +0000

    Use longer variable names to be more explicit

--- lib/combine_popolo_memberships.rb
+++ lib/combine_popolo_memberships.rb
@@ -2,26 +2,26 @@ require 'combine_popolo_memberships/version'
 
 # Takes multiple popolo membership arrays and combines them based on date.
 module CombinePopoloMemberships
-  def self.overlap(mem, term)
-    mS = mem[:start_date].to_s.empty? ? '0000-00-00' : mem[:start_date].to_s
-    mE = mem[:end_date].to_s.empty? ? '9999-99-99' : mem[:end_date].to_s
-    tS = term[:start_date].to_s.empty? ? '0000-00-00' : term[:start_date].to_s
-    tE = term[:end_date].to_s.empty? ? '9999-99-99' : term[:end_date].to_s
+  def self.overlap(membership, term)
+    membership_start_date = membership[:start_date].to_s.empty? ? '0000-00-00' : membership[:start_date].to_s
+    membership_end_date = membership[:end_date].to_s.empty? ? '9999-99-99' : membership[:end_date].to_s
+    term_start_date = term[:start_date].to_s.empty? ? '0000-00-00' : term[:start_date].to_s
+    term_end_date = term[:end_date].to_s.empty? ? '9999-99-99' : term[:end_date].to_s
 
-    return unless mS < tE && mE > tS
-    (s, e) = [mS, mE, tS, tE].sort[1, 2]
+    return unless membership_start_date < term_end_date && membership_end_date > term_start_date
+    start_date, end_date = [membership_start_date, membership_end_date, term_start_date, term_end_date].sort[1, 2]
     {
-      _data: [mem, term],
-      start_date: s == '0000-00-00' ? nil : s,
-      end_date:   e == '9999-99-99' ? nil : e
+      _data: [membership, term],
+      start_date: start_date == '0000-00-00' ? nil : start_date,
+      end_date:   end_date == '9999-99-99' ? nil : end_date
     }
   end
 
-  def self.combine(h)
-    into_name, into_data, from_name, from_data = h.flatten
-    from_data.product(into_data).map { |a, b| overlap(a, b) }.compact.map do |h|
-      data = h.delete :_data
-      h.merge(from_name => data.first[:id], into_name => data.last[:id])
+  def self.combine(memberships)
+    into_name, into_data, from_name, from_data = memberships.flatten
+    from_data.product(into_data).map { |a, b| overlap(a, b) }.compact.map do |membership|
+      data = membership.delete :_data
+      membership.merge(from_name => data.first[:id], into_name => data.last[:id])
     end.sort_by { |h| h[:start_date] }
   end
 end
--- test/combine_popolo_memberships_test.rb
+++ test/combine_popolo_memberships_test.rb
@@ -5,14 +5,14 @@ class CombinePopoloMembershipsTest < Minitest::Test
     refute_nil ::CombinePopoloMemberships::VERSION
   end
 
-  def term_mems
-    @term_mems ||= [
+  def term_memberships
+    [
       { id: '2', name: '2. Národná rada 1998-2002', start_date: '1998-09-26', end_date: '2002-09-21' }
     ]
   end
 
-  def group_mems
-    @group_mems ||= [
+  def group_memberships
+    [
       { name: 'Independent', id: '0', end_date: Date.new(1998, 10, 28) },
       { id: '1', name: 'Klub ĽS-HZDS', start_date: Date.new(1998, 10, 29), end_date: Date.new(2002, 07, 15) },
       { name: 'Independent', id: '0', start_date: Date.new(2002, 07, 16) }
@@ -25,6 +25,6 @@ class CombinePopoloMembershipsTest < Minitest::Test
       { start_date: '1998-10-29', end_date: '2002-07-15', faction_id: '1', term: '2' },
       { start_date: '2002-07-16', end_date: '2002-09-21', faction_id: '0', term: '2' }
     ]
-    assert_equal expected, CombinePopoloMemberships.combine(term: term_mems, faction_id: group_mems)
+    assert_equal expected, CombinePopoloMemberships.combine(term: term_memberships, faction_id: group_memberships)
   end
 end
