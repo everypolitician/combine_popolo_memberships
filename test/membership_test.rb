bfc873d2bfbd966b7cb5a07703fbeadda923c153
Chris Mytton <chrism@mysociety.org>
2017-04-21 16:51:46 +0100

    Use date_range to get to dates in tests
    This means we can have a smaller public interface on the membership
    because we won't have to expose the start date and end date methods.

--- test/membership_test.rb
+++ test/membership_test.rb
@@ -3,20 +3,20 @@ require 'test_helper'
 class MembershipTest < Minitest::Test
   def test_with_known_dates
     membership = CombinePopoloMemberships::Membership.new(start_date: '2015-11-27', end_date: '2015-12-31')
-    assert_equal '2015-11-27', membership.start_date
-    assert_equal '2015-12-31', membership.end_date
+    assert_equal '2015-11-27', membership.date_range.start_date
+    assert_equal '2015-12-31', membership.date_range.end_date
   end
 
   def test_with_nil
     membership = CombinePopoloMemberships::Membership.new(start_date: nil, end_date: nil)
-    assert_equal '0000-00-00', membership.start_date
-    assert_equal '9999-99-99', membership.end_date
+    assert_equal '0000-00-00', membership.date_range.start_date
+    assert_equal '9999-99-99', membership.date_range.end_date
   end
 
   def test_with_empty_strings
     membership = CombinePopoloMemberships::Membership.new(start_date: '', end_date: '')
-    assert_equal '0000-00-00', membership.start_date
-    assert_equal '9999-99-99', membership.end_date
+    assert_equal '0000-00-00', membership.date_range.start_date
+    assert_equal '9999-99-99', membership.date_range.end_date
   end
 
   def test_returns_original_membership
